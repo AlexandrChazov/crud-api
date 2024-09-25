c2745c1a73d96ad9bf93996001127dd9ec38d79b
Alexandr Chazov <chazovby@gmail.com>
2024-09-25 10:35:25 +0300

    feat: create GET api/users endpoint

--- src/lib/start.ts
+++
@@ -1,4 +0,0 @@
-import { arch } from "os";
-export function start() {
-	process.stdout.write(arch());
-}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,24 @@
-import { start } from "./lib";
+import { createServer } from "http";
+import { readFile } from "fs";
 
-start();
+const server = createServer((req, res) => {
+	if (req.method === "GET" && req.url === "/api/users") {
+		readFile("src/db/users.json", "utf-8", (err, data) => {
+			if (err) {
+				console.log(err);
+			} else {
+				const users = JSON.parse(data);
+				res.writeHead(200, { "Content-type": "text/plain" });
+				res.end(JSON.stringify(Object.values(users)));
+			}
+		});
+	} else {
+		res.writeHead(404, { "Content-type": "text/plain" });
+		res.end("The endpoint doesn't exist");
+	}
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+	console.log(`Server running at port ${PORT}`);
+});
